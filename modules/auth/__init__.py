5377b1bcd173eae7e43b646a4c11fbe3ef1745e1
roderick20 <roderick20@hotmail.com>
2025-11-24 05:48:42 -0500

    1

--- modules/auth/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-bp = Blueprint('auth', __name__, template_folder='templates')
-
-from .routes import auth_routes, main_routes, usuario_routes
--- modules/ordenes/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-bp = Blueprint('orden', __name__, template_folder='templates')
-
-from .routes import orden_routes
